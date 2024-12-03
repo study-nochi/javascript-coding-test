49d0e054e5b8f850af3f9f277c262617afaf712d
ainochi-kor <kthrkdals@gmail.com>
2024-12-04 00:59:42 +0900

    백준 18870: 좌표 압축

---
+++ sort/18870.js
@@ -0,0 +1,16 @@
+let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+let input = `5
+2 4 -10 4 -9`.toString().split('\n');
+
+let arr = input[1].split(' ').map(Number);
+let uniqueArr = Array.from(new Set(arr)).sort((a, b) => a - b);
+
+let map = new Map();
+for (let i = 0; i < uniqueArr.length; i++) {
+  map.set(uniqueArr[i], i);
+}
+
+const answer = arr.map(num => map.get(num))
+
+console.log(answer.join(' '))
